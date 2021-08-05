181343a0d216322c3b8bb8744bc12dca1c5c8e60
phpitcast <88470748+phpitcast@users.noreply.github.com>
2021-08-05 15:45:40 +0800

    Update index5.php

--- index5.php
+++ index5.php
@@ -1,2 +1,10 @@
 <?php
 echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
+echo "这是index5.php文件";
