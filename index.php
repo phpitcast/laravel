9f3c45d6073110ff53245912d48d5dcab2623f2e
phpitcast <13809742530@163.com>
2021-08-05 12:06:38 +0800

    本地的第一次提交

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/8/5 0005
+ * Time: 11:58
+ */
+echo "hello word";
---
+++ index3.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/8/5 0005
+ * Time: 12:00
+ */
+echo "php74";
